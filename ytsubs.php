d8ea299569ac1e3e1add532e703c3270abb51887
Jared Matta <cherokeedreemer@gmail.com>
2022-06-09 13:31:52 -0600

    finished

--- ytsubs.php
+++ ytsubs.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: YouTube Subs
+Plugin Name: youtubesubs
 Plugin URI: https://
 Description: Counts and displays current YouTube subscriptions.
 Version: 0.1.0
@@ -10,8 +10,17 @@ Text Domain:
 Domain Path:
 */
 
-// Exit if accessed directly
+// Exit if path is accessed directly
 if(!defined('ABSPATH')){exit;}
 
 // Load scripts
 require_once(plugin_dir_path(__FILE__).'/includes/ytsubs-scripts.php');
+
+// Load Class
+require_once(plugin_dir_path(__FILE__).'/includes/ytsubs-class.php');
+
+// Register Widget
+function register_youtubesubs(){register_widget('Youtube_Subs_Widget');}
+
+// Hook in function
+add_action('widgets_init', 'register_youtubesubs');
